d39dd3f7950e230d56a2f1f00de169ac394c50a4
wecansur <vlkob95@gmail.com>
2023-01-19 12:48:38 +0200

    feat: copy previous solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const binaryArray = [];
+
+    for (let i = 0; i < expr.length; i = i + 10) {
+        binaryArray.push(expr.slice(i, i + 10))
+    };
+    
+    const dividedArray = [];
+
+    binaryArray.forEach((item,) => {
+        if (item === '**********') {
+            dividedArray.push(item)
+        } else {
+            let result =[];
+            for (let i = 0; i < item.length; i = i + 2) {
+                result.push(item.slice(i, i + 2));
+            }
+            dividedArray.push(result)
+        }
+    })
+    
+    const morseArray = [];
+    
+    dividedArray.forEach((item) => {
+        if (item === '**********') {
+            morseArray.push(item);
+        } else {
+            let result = '';
+            item.forEach((innerItem) => {
+                if (innerItem === '10') {
+                    result += '.'
+                } else if (innerItem === '11') {
+                    result += '-'
+                }
+            })
+            morseArray.push(result);
+        }
+    })
+    
+    let result = '';
+
+    morseArray.forEach((item) => {
+        if (item === '**********') {
+            result += ' '
+        } else {
+            result += MORSE_TABLE[item]
+        }
+    })    
+    return result
 }
 
 module.exports = {
